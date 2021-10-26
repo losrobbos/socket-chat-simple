eb8b0e3510b05e4a61bd3e3f1bdc897bbc379cca
losrobbos <losrobbos@web.de>
2021-10-26 11:14:03 +0200

    Added full sample fullstack

--- chat-ui/src/App.js
+++ chat-ui/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import { useEffect, useRef, useState } from 'react';
 import faker from 'faker'
+import socketIo from 'socket.io-client'
 
 // URL to API / Message Server
 const MESSAGE_SERVER_URL = process.env.REACT_APP_MESSAGE_SERVER_URL
 
 function App() {
+
+  // state vs refs FIGHT
+  // state => RE-RENDERS !
+  // ref => do NOT trigger RE-RENDER !
+
+  const socketRef = useRef() // global variables
 
   // generate some fake user....
   const [userName, setUsername] = useState( faker.name.firstName() )
@@ -19,20 +26,50 @@ function App() {
   // store here the message that we typed...
   const msgRef = useRef() 
 
-  // initialize socket connection on load
   useEffect(() => {
+    // initialize socket connection on load
+    socketRef.current = socketIo( MESSAGE_SERVER_URL )
+
 
   }, [])
+
+  // on every change of history 
+  // => re-register message listener!
+  useEffect(() => {
+    const socket = socketRef.current
+
+    // custom event listener => not triggered in DOM!
+    socket.on("message", (msg) => {
+      console.log("Received somethin': ", msg)
+      setChatHistory([...chatHistory, msg]) // chatHistory contains a SNAPSHOT of state when this listener was created
+    })
+
+    // cleanup => remove event listener on every new message and re-assign
+    return () => {
+      socketRef.current && socketRef.current.off("message")
+    }
+
+  }, [ chatHistory ] )
   
 
   // add our message to history - and emit it to all our peeeeeps
   const addChatMessageToHistory = (e) => {
     e.preventDefault()
 
+    // msgRef.current => gives me the DOM node => <input />
+    // msgRef.current.value => value of input field
     let chatMsg = { text: msgRef.current.value, user: userName }
     msgRef.current.value = "" // clear input box
 
-    setChatHistory([ ...chatHistory, chatMsg ])    
+    // send to socket.io // => send it to everyone else connected
+
+    const socket = socketRef.current
+
+    // socket.on("")
+    // socket.emit("")
+    socket.emit("message", chatMsg) // emit event "message" to socket io server
+
+    setChatHistory([ ...chatHistory, chatMsg ])
   }
  
   // create JSX history from chat entries
@@ -56,7 +93,8 @@ function App() {
         <form id="message-send" onSubmit={ addChatMessageToHistory }>
           <input
             autoComplete="off"
-            ref={msgRef}
+            ref={msgRef} // we are not interested in intermediate typing results
+            // value={msg} onChange={setMsg()} // => validation as you type!
             name="message-new"
             placeholder={`Type your message, ${userName}...`} 
           />
--- server.js
+++ server.js
@@ -1,9 +1,57 @@
 import express from "express"
+import { Server } from 'socket.io' 
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config() // load env vars
+
+// requirement env check at startup
+if(!process.env.FRONTEND_ORIGIN) {
+  console.log("Necessary env config missing. Expected FRONTEND_ORIGIN")
+  process.exit() // terminate API script before it even starts up
+}
+
 const app = express() // create API instance
+
+app.use((req, res, next) => {
+  console.log("API request was made...")
+  next()
+})
 
 app.get("/", (req, res, next) => {
   res.json({ message: "Hello from Message Server (we use Socket.io, buddy)"})
 })
 
+// setup cors for API
+app.use( cors({ origin: process.env.FRONTEND_ORIGIN }) ) // ==> will just affect API routes!
+
 const PORT = process.env.PORT || 5000
-const server = app.listen(PORT, () => console.log(`API started on http://localhost:${PORT}`))
+
+// Startup of HTTP server
+const httpServer = app.listen(PORT, () => console.log(`API started on http://localhost:${PORT}`))
+
+// Hook in Socket.io server into Express API
+const io = new Server( httpServer, 
+  { cors: { origin: process.env.FRONTEND_ORIGIN } } 
+) // io = Socket server
+
+// socket => individual connection of client & server
+// browser tab 1 => own socket
+// browser tab 2 => own socket
+io.on("connection", ( socket ) => {
+
+  console.log("Someone connected. So exciting...")
+
+  // setup an event listener 
+  // message = direct line channel
+  socket.on("message", (data) => {
+    console.log("Received some stuff: ", data)
+
+    // forward message to everyone else
+    socket.broadcast.emit("message", data)
+  })
+
+
+})
+
+
